2a8a7afe34f999b97a1cab7a1e756907ef42d66e
bayer <bayernator@protonmail.com>
2023-07-16 09:54:51 +0200

    continue work on beginner page - styling/animation - fix buttons on guideselector
    Signed-off-by: bayer <bayernator@protonmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,33 @@ module.exports = {
   },
   theme: {
     extend: {
-     animation: {
-         'fadein': 'fadein 2s ease-in-out forwards',
-        },
-    keyframes: {
-        fadein: {
-            '0%': { opacity: 0},
-            '100%': { opacity: 1},
-        },
+      animation: {
+          'fadein': 'fadein 2s ease-in-out forwards',
+          'fadeinone': 'fadeinone 1s ease-in-out forwards',
+          'slideout': 'slideout 1s ease-in-out',
+          'slidein': 'slidein 1s ease-in-out',
+         },
+      keyframes: {
+          fadein: {
+              '0%': { opacity: 0},
+              '100%': { opacity: 1},
+          },
+          fadeinone: {
+              '0%': { opacity: 0},
+              '100%': { opacity: 1},
+          },
+          slideout: {
+              '0%': { transform: 'translateX(0%)', opacity: 1 },
+              '99.999%': {opacity: 0},
+              '100%' : { transform: 'translateX(-100%)', opacity: 1 },
+          },
+          slidein: {
+              '0%': { transform: 'translateX(100%)', opacity: 1 },
+              '100%' : { transform: 'translateX(0%)', opacity: 1 },
+          },
+      },
     },
   },
-},
   plugins: [],
 }
 
