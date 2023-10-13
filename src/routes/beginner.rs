a90619d93f4c0de4a2e6e96b035373193a240db1
bayer <bayernator@protonmail.com>
2023-10-14 00:18:30 +0200

    test faq conversion
    Signed-off-by: bayer <bayernator@protonmail.com>

--- src/routes/beginner.rs
+++ src/routes/beginner.rs
@@ -274,8 +274,21 @@ pub fn BeginnerWalletInstructions(cx: Scope, blue: bool, samourai: bool, green: 
 
         let green_apple_store = r"https://apps.apple.com/us/app/green-bitcoin-wallet/id1402243590".to_string();;
         
-        let samourai_faqs = FAQS { titles: vec!("title1".to_string(), "title2".to_string(), "title3".to_string()), 
-                                contents: vec!("string_1".to_string(), "string_2".to_string(), "string_2".to_string())}; 
+        let samourai_faqs = FAQS { titles: vec!("## How do I create a wallet?".to_string(), "## First Wallet Setup Guide".to_string(), "## How do I receive bitcoin?".to_string()), 
+                                contents: vec!("After installing and opening the app, select **mainnet** and press **continue**.
+
+Follow the prompts to create your wallet. Make sure to carefully read the instructions and
+take your time. Understanding the process is as important as taking self custody of your
+bitcoin.
+
+Since you’re just starting out on your self-custody journey let's keep things simple. If
+you’re asked to make a decision regarding features that you do not understand, **for
+example: turn on Tor** simply leave it on the default value and move on. The intermediate
+guide will dive deeper into those options.".to_string(),
+
+                                               "string_2".to_string(), "string_2".to_string())}; 
+
+
         
         //let (show_content, set_show_content) = create_signal(cx, false);
         //
@@ -315,7 +328,7 @@ pub fn BeginnerWalletInstructions(cx: Scope, blue: bool, samourai: bool, green: 
                         </Show>        
                     </div>
                     
-                    <h2 class="flex justify-center font-bold text-2xl text-white pt-6">"Help Me!"</h2>
+                    <h2 class="flex justify-center font-bold text-2xl text-white pt-6 pb-2">"Help Me!"</h2>
                     <AccordionMenu faqs=samourai_faqs/>
                 </div>
             }
@@ -339,7 +352,7 @@ pub fn BeginnerWalletInstructions(cx: Scope, blue: bool, samourai: bool, green: 
                         <DownloadButton href=samourai_android_apk.clone() logo=img_url_github.clone() alt_txt=img_alt_github.clone() button_name="Android APK".to_string()/>
                     </div>
 
-                    <h2 class="flex justify-center font-bold text-2xl text-white pt-6">"Help Me!"</h2>
+                    <h2 class="flex justify-center font-bold text-2xl text-white pt-6 pb-2">"Help Me!"</h2>
                     <AccordionMenu faqs=samourai_faqs/>
                 </div>
             }
@@ -361,7 +374,7 @@ pub fn BeginnerWalletInstructions(cx: Scope, blue: bool, samourai: bool, green: 
                         <DownloadButton href=green_apple_store.clone() logo=apple_store_logo_2.clone() alt_txt=apple_store_alt.clone()/>
                     </div>
 
-                    <h2 class="flex justify-center font-bold text-2xl text-white pt-6">"Help Me!"</h2>
+                    <h2 class="flex justify-center font-bold text-2xl text-white pt-6 pb-2">"Help Me!"</h2>
                     <AccordionMenu faqs=samourai_faqs/>
                 </div>
                 }
