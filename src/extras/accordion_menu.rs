dd63a5695583a73a2fcdbc51827976721cd72eab
bayer <bayernator@protonmail.com>
2023-10-25 12:51:07 +0200

    run cargo fmt
    Signed-off-by: bayer <bayernator@protonmail.com>

--- src/extras/accordion_menu.rs
+++ src/extras/accordion_menu.rs
@@ -1,34 +1,27 @@
-use leptos::*;
-use leptos::logging::log;
 use crate::server::api::fetch_faqs::*;
-use pulldown_cmark::{Parser, Options, html};
+use leptos::logging::log;
+use leptos::*;
+use pulldown_cmark::{html, Options, Parser};
 
 #[allow(non_snake_case)]
 fn MarkdownToHtml(markdown: &str) -> String {
-
     let mut options = Options::empty();
     options.insert(Options::ENABLE_STRIKETHROUGH);
     options.insert(Options::ENABLE_SMART_PUNCTUATION);
     options.insert(Options::ENABLE_FOOTNOTES);
     let parser = Parser::new_ext(markdown, options);
- 
+
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
 
     html_output
-
 }
 
-// FAQ accordion menu button 
+// FAQ accordion menu button
 #[component]
 #[allow(non_snake_case)]
-fn Menu(
-    faq_title: String,
-    faq_content: String,
-) -> impl IntoView
-{
+fn Menu(faq_title: String, faq_content: String) -> impl IntoView {
     let (menu_clicked, set_menu_clicked) = create_signal(false);
-    
 
     // takes faq_content and faq_title to make a button and a accordion style container
     view! {
@@ -51,63 +44,55 @@ fn Menu(
     }
 }
 
-// Accordion menu component for faqs, creates necessary number of Menu comps based on props passed.  
+// Accordion menu component for faqs, creates necessary number of Menu comps based on props passed.
 #[component]
 #[allow(non_snake_case)]
 pub fn AccordionMenu(#[prop(optional)] faqs: String) -> impl IntoView {
-    
-      let markdown = MarkdownToHtml("![note] Hello, *there*");
-      
-      let new_faqs = create_resource( 
-            move || (),
-            move |_| fetch_faq(faqs.clone())
-            );
+    let markdown = MarkdownToHtml("![note] Hello, *there*");
 
-     // let faqs_test = match &new_faqs.get() {
-     //     Some(Ok(faq_vecs)) =>  log!("markdown: {}", MarkdownToHtml(&faq_vecs[0].content)),
-     //     None => log!("Nothing to see here"),
-     //     Some(Err(error)) => log!("Error rendering faqs: {}", error)
-     //     
-     // };
-    
+    let new_faqs = create_resource(move || (), move |_| fetch_faq(faqs.clone()));
 
-      //log!("markdown {:#?}", markdown);
+    // let faqs_test = match &new_faqs.get() {
+    //     Some(Ok(faq_vecs)) =>  log!("markdown: {}", MarkdownToHtml(&faq_vecs[0].content)),
+    //     None => log!("Nothing to see here"),
+    //     Some(Err(error)) => log!("Error rendering faqs: {}", error)
+    //
+    // };
 
-      view! {
-        <div id="accordion-collapse" data-accordion="collapse">
-            <Suspense
-                fallback=move || view! { <div>"Loading...."</div> }
-            >
-                {move || {
-                    match new_faqs.get() {
-                        None => {
-                            view! { <div>"No Data Available"</div> }
-                        }
-                        Some(Ok(faq_vec)) => {
-                            view! { 
-                                <div>
-                                    <For 
-                                        each=move || faq_vec.clone()
-                                        key= |faqs| faqs.id
-                                        children=move |faqs| {
-                                            view! {<Menu faq_title=MarkdownToHtml(&faqs.title) faq_content=MarkdownToHtml(&faqs.content)/>}
-                                        }
-                                    />
-                                </div>
-                            }
-                        }
-                        Some(Err(error)) => {
-                            log!("Error rendering faqs: {}", error);
-                            view! { <div>
-                                    "Oops we ran into an error"
-                                    </div> }
+    //log!("markdown {:#?}", markdown);
+
+    view! {
+    <div id="accordion-collapse" data-accordion="collapse">
+        <Suspense
+            fallback=move || view! { <div>"Loading...."</div> }
+        >
+            {move || {
+                match new_faqs.get() {
+                    None => {
+                        view! { <div>"No Data Available"</div> }
+                    }
+                    Some(Ok(faq_vec)) => {
+                        view! {
+                            <div>
+                                <For
+                                    each=move || faq_vec.clone()
+                                    key= |faqs| faqs.id
+                                    children=move |faqs| {
+                                        view! {<Menu faq_title=MarkdownToHtml(&faqs.title) faq_content=MarkdownToHtml(&faqs.content)/>}
+                                    }
+                                />
+                            </div>
                         }
                     }
-                }}
-            </Suspense>
-        </div>
-        }
-        
+                    Some(Err(error)) => {
+                        log!("Error rendering faqs: {}", error);
+                        view! { <div>
+                                "Oops we ran into an error"
+                                </div> }
+                    }
+                }
+            }}
+        </Suspense>
+    </div>
+    }
 }
-       
-
