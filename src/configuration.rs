29a3b4274286ea19feb8334e1289aa725ae9dd03
bayer <bayernator@protonmail.com>
2024-02-01 23:30:05 +0100

    run cargo fmt
    Signed-off-by: bayer <bayernator@protonmail.com>

--- src/configuration.rs
+++ src/configuration.rs
@@ -17,17 +17,20 @@ impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
             "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password, self.host, self.port, self.database_name
-            )
+            self.username,
+            self.password,
+            self.host,
+            self.port,
+            self.database_name
+        )
     }
 
     pub fn connection_string_without_db(&self) -> String {
         format!(
             "postgres://{}:{}@{}:{}",
             self.username, self.password, self.host, self.port
-            )
-    } 
-    
+        )
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
--- src/server/create_post.rs
+++ src/server/create_post.rs
@@ -1,10 +1,9 @@
-#[cfg(feature="ssr")]
+#[cfg(feature = "ssr")]
 use actix_web::{web, HttpResponse};
-#[cfg(feature="ssr")]
+#[cfg(feature = "ssr")]
 use sqlx::PgPool;
 
-
-#[cfg(feature="ssr")]
+#[cfg(feature = "ssr")]
 #[derive(serde::Deserialize)]
 pub struct BlogpostData {
     author: String,
@@ -12,15 +11,14 @@ pub struct BlogpostData {
     content: String,
 }
 
-#[cfg(feature="ssr")]
+#[cfg(feature = "ssr")]
 pub async fn create_post(
-    blogpost_data: web::Json<BlogpostData>, 
+    blogpost_data: web::Json<BlogpostData>,
     connection: web::Data<PgPool>,
 ) -> HttpResponse {
-
     use chrono::Utc;
     use uuid::Uuid;
-    
+
     match sqlx::query!(
         r#"
         INSERT INTO blogposts (id, created_at, author, title, content)
@@ -44,4 +42,3 @@ pub async fn create_post(
         }
     }
 }
-
