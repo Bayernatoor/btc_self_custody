4abb6f00b6609c3380069cd3fa75104524e81ecd
bayer <bayernator@protonmail.com>
2024-02-12 19:28:40 +0100

    Add desktop guide
    1. Add desktop button to guide selector
    2. add Sparrow wallet to desktop
    3. update some routes & styles
    
    Signed-off-by: bayer <bayernator@protonmail.com>

--- src/app.rs
+++ src/app.rs
@@ -7,6 +7,7 @@ use crate::routes::faq::*;
 use crate::routes::guideselector::*;
 use crate::routes::homepage::*;
 use crate::routes::intermediate::*;
+use crate::routes::not_found::*;
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
@@ -25,7 +26,8 @@ pub fn App() -> impl IntoView {
         <Title text="Bitcoin Self Custody"/>
 
         // sets the body background color throughout the app
-        <Body class="bg-[#1a578f]"/>
+        //<Body class="bg-[#1a578f]"/>
+        <Body class="bg-[#123c64]"/>
 
         // Routes
         <Router>
@@ -34,19 +36,23 @@ pub fn App() -> impl IntoView {
                 <Routes>
                     <Route path="/" view=|| view! {<HomePage/> }/>
                     <Route path="/guides" view=|| view! {<GuideSelector/> }/>
+                    // Basic guide routes
+                    <Route path="/guides/basic/desktop" view=|| view! {<RenderDesktopPage/>}/>
+                    <Route path="/guides/basic/desktop/sparrow" view=|| view! {<BeginnerWalletInstructions selected_wallet=WalletName::Sparrow ios=false/>}/>
                     <Route path="/guides/basic/android" view=|| view! {<RenderAndroidPage/>}/>
                     <Route path="/guides/basic/android/samourai" view=|| view! {<BeginnerWalletInstructions selected_wallet=WalletName::Samourai ios=false/>}/>
-                    <Route path="/guides/basic/android/blue" view=|| view! {<BeginnerWalletInstructions selected_wallet=WalletName::Blue ios=false/>}/>
+                    <Route path="/blue" view=|| view! {<BeginnerWalletInstructions selected_wallet=WalletName::Blue ios=false/>}/>
                     <Route path="/guides/basic/ios" view=|| view! {<RenderIosPage/> }/>
-                    <Route path="/guides/basic/ios/blue" view=|| view! {<BeginnerWalletInstructions selected_wallet=WalletName::Blue ios=true/>}/>
-                    <Route path="/guides/basic/ios/blockstream" view=|| view! {<BeginnerWalletInstructions selected_wallet=WalletName::Green ios=true/>}/>
-                    <Route path="/guides/intermediate/android" view=|| view! {<IntermediatePage/> }/>
-                    <Route path="/guides/intermediate/ios" view=|| view! {<IntermediatePage/> }/>
-                    <Route path="/guides/intermediate/desktop" view=|| view! {<IntermediatePage/> }/>
+                        <Route path="/blue" view=|| view! {<BeginnerWalletInstructions selected_wallet=WalletName::Blue ios=true/>}/>
+                        <Route path="/blockstream" view=|| view! {<BeginnerWalletInstructions selected_wallet=WalletName::Green ios=true/>}/>
+                    // Intermediate guide routes 
+                    <Route path="/guides/intermediate" view=|| view! {<IntermediatePage/> }/>
+                    // Advanced guide routes 
                     <Route path="/guides/advanced" view=|| view! {<AdvancedPage/> }/>
                     <Route path="/blog" view=|| view! {<BlogPage/> }/>
                     <Route path="/faq" view=|| view! {<FaqPage/> }/>
                     <Route path="/about" view=|| view! {<AboutPage/> }/>
+                    //<Route path="/*" view=|| view! {<NotFound/> }/>
                 </Routes>
             </main>
         </Router>
---
+++ src/routes/not_found.rs
@@ -0,0 +1,18 @@
+//use leptos::*;
+//use crate::extras::back::BackButton;
+//
+///// Renders the About page of the application.
+//#[component]
+//pub fn NotFound() -> impl IntoView {
+//    view! {
+//        <div id="about" class="flex flex-col max-w-3xl mx-auto rounded-xl pb-10 animate-fadeinone">
+//            <div class="flex flex-col p-6 pt-10 max-w-3xl mx-auto">
+//                    <h1 class="flex justify-center text-[36px] text-white font-semibold">"This Page Doesn't Exist"</h1>
+//                <div class="flex justify-center pt-4 max-w-sm">
+//                    <p class="text-sm text-white">"Go Home"</p>
+//                </div>
+////                <BackButton _location="/home".to_string() button_image="./../../../golden_home.png".to_string()/>
+//            </div>
+//        </div>
+//    }
+//}
