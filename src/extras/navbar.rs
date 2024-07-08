915b3868ac2cb9710ccea0bde36ec719dcade269
bayer <bayernator@protonmail.com>
2024-07-08 14:59:54 +0200

    wrap svg in a div
    Signed-off-by: bayer <bayernator@protonmail.com>

--- src/extras/navbar.rs
+++ src/extras/navbar.rs
@@ -1,17 +1,12 @@
 use leptos::*;
 
-#[derive(Copy, Clone, Debug)]
-pub struct NavbarContext {
-    pub guide: WriteSignal<bool>,
-}
-
 #[allow(clippy::redundant_closure)]
 #[component]
 pub fn NavBar() -> impl IntoView {
     let (menu_clicked, set_menu_clicked) = create_signal(false);
 
     view! {
-        <div class="bg-[#123c64] text-white sticky top-0 z-20 w-full mx-auto p-8 flex justify-between items-center">
+        <div class="bg-[#123c64] text-white sticky top-0 z-10 w-full mx-auto p-8 flex justify-between items-center">
             <div>
                 <div class="text-3xl lg:text-4xl font-medium text-white font-title">
                     <a href="/">"WE HODL BTC"</a>
@@ -32,28 +27,30 @@ pub fn NavBar() -> impl IntoView {
                 </a>
             </div>
             <div class="flex lg:hidden cursor-pointer" on:click=move |_| set_menu_clicked.update(|value| *value = !*value)>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-8 h-8 transition-transform duration-300 transform"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M4 6h16M4 12h16M4 18h16"
-                        class:hidden={move || menu_clicked()}
-                    />
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M6 18L18 6M6 6l12 12"
-                        class:hidden={move || !menu_clicked()}
-                    />
-                </svg>
+                <div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-8 h-8 transition-transform duration-300 transform"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16"
+                            class:hidden={move || menu_clicked()}
+                        />
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M6 18L18 6M6 6l12 12"
+                            class:hidden={move || !menu_clicked()}
+                        />
+                    </svg>
+                </div>
             </div>
         </div>
         // TODO: add event listener to body of app to listen for click when menu is open
