ab4c8715fdebf6406eca64001d923133aeb482ae
bayer <bayernator@protonmail.com>
2024-07-09 11:32:56 +0200

    some tweaks to navbar and format a bunch of componets
    Signed-off-by: bayer <bayernator@protonmail.com>

--- src/extras/generic_button.rs
+++ src/extras/generic_button.rs
@@ -27,13 +27,19 @@ pub fn GenericButton(
     view! {
         <a href=path>
             <button class="flex justify-center shrink-0 h-18 w-72 p-2 mx-auto bg-white rounded-xl items-center hover:bg-[#f2f2f2]">
-              <div class="flex justify-center basis-1/3 shrink-0">
-                <img class=format!("h-{} w-{}", height.get(), width.get()) src=img_url alt=img_alt/>
-              </div>
-              <div class="basis-2/3">
-                <h3 class=format!("text-{text_size} font-semibold text-[{text_color}]")>{wallet_title}</h3>
-                <p class="text-slate-500">{short_desc}</p>
-              </div>
+                <div class="flex justify-center basis-1/3 shrink-0">
+                    <img
+                        class=format!("h-{} w-{}", height.get(), width.get())
+                        src=img_url
+                        alt=img_alt
+                    />
+                </div>
+                <div class="basis-2/3">
+                    <h3 class=format!(
+                        "text-{text_size} font-semibold text-[{text_color}]",
+                    )>{wallet_title}</h3>
+                    <p class="text-slate-500">{short_desc}</p>
+                </div>
             </button>
         </a>
     }
@@ -65,13 +71,19 @@ pub fn GenericExternalButton(
     view! {
         <a href=path rel="noreferrer" target="_blank" rel="noreferrer">
             <button class="flex flex-col justify-center h-auto w-72 p-2 mx-auto bg-white rounded-xl items-center hover:bg-[#f2f2f2]">
-              <div class="flex justify-center basis-1/2">
-                <img class=format!("h-{} w-{}", height.get(), width.get()) src=img_url alt=img_alt/>
-              </div>
-              <div class="basis-1/2">
-                <p class=format!("text-md mt-1.5 font-semibold text-[{text_color}]")>{wallet_title}</p>
-                <p class="text-slate-500">{short_desc}</p>
-              </div>
+                <div class="flex justify-center basis-1/2">
+                    <img
+                        class=format!("h-{} w-{}", height.get(), width.get())
+                        src=img_url
+                        alt=img_alt
+                    />
+                </div>
+                <div class="basis-1/2">
+                    <p class=format!(
+                        "text-md mt-1.5 font-semibold text-[{text_color}]",
+                    )>{wallet_title}</p>
+                    <p class="text-slate-500">{short_desc}</p>
+                </div>
             </button>
         </a>
     }
@@ -92,11 +104,11 @@ pub fn GenericImageSubTextButton(
     view! {
         <a href=path rel="noreferrer" target="_blank">
             <button class="flex justify-center shrink-0 h-18 w-72 p-2 mx-auto bg-white rounded-xl items-center space-x-4 shadow-inner hover:bg-[#f2f2f2]">
-              <div>
-                //<img class=format!("h-{} w-{} inline-flex", height.get(), width.get()) src=img_url alt=img_alt/>
-                <h3 class=format!("text-lg font-medium text-[{text_color}]")>{title}</h3>
-                <p class="text-slate-600">{short_desc}</p>
-              </div>
+                <div>
+                    // <img class=format!("h-{} w-{} inline-flex", height.get(), width.get()) src=img_url alt=img_alt/>
+                    <h3 class=format!("text-lg font-medium text-[{text_color}]")>{title}</h3>
+                    <p class="text-slate-600">{short_desc}</p>
+                </div>
             </button>
         </a>
     }
